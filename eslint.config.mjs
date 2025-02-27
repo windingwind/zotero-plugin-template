d1c68ab58877386cfe9cd3c2d866bc95561b8e25
Northword <northword@outlook.com>
2025-02-27 21:37:42 +0800

    fix: bump eslint-config to get rules' types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,25 +2,26 @@
 
 import zotero from "@zotero-plugin/eslint-config";
 
-export default [
-  ...zotero(),
-  {
-    files: ["**/*.ts"],
-    rules: {
-      "no-restricted-globals": [
-        "error",
-        { message: "Use `Zotero.getMainWindow()` instead.", name: "window" },
-        {
-          message: "Use `Zotero.getMainWindow().document` instead.",
-          name: "document",
-        },
-        {
-          message: "Use `Zotero.getActiveZoteroPane()` instead.",
-          name: "ZoteroPane",
-        },
-        "Zotero_Tabs",
-      ],
-      "@typescript-eslint/no-unused-vars": "off",
+export default zotero({
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "no-restricted-globals": [
+          "error",
+          { message: "Use `Zotero.getMainWindow()` instead.", name: "window" },
+          {
+            message: "Use `Zotero.getMainWindow().document` instead.",
+            name: "document",
+          },
+          {
+            message: "Use `Zotero.getActiveZoteroPane()` instead.",
+            name: "ZoteroPane",
+          },
+          "Zotero_Tabs",
+        ],
+      },
     },
-  },
-];
+  ],
+});
