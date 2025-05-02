db13a1184ac5dc2fbb49448156c00de633863b6f
Northword <northword@outlook.com>
2025-05-02 14:25:42 +0800

    fix: remove no-restricted-globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,19 +8,6 @@ export default zotero({
       files: ["**/*.ts"],
       rules: {
         "@typescript-eslint/no-unused-vars": "off",
-        "no-restricted-globals": [
-          "error",
-          { message: "Use `Zotero.getMainWindow()` instead.", name: "window" },
-          {
-            message: "Use `Zotero.getMainWindow().document` instead.",
-            name: "document",
-          },
-          {
-            message: "Use `Zotero.getActiveZoteroPane()` instead.",
-            name: "ZoteroPane",
-          },
-          "Zotero_Tabs",
-        ],
       },
     },
   ],
